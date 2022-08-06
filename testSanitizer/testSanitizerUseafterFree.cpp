8d29f9b9121023d9b12582dca9c4436d2b5a7a83
sharpenHolic <17780547892@163.com>
2022-08-06 12:50:35 +0800

    update some files

---
+++ testSanitizer/testSanitizerUseafterFree.cpp
@@ -0,0 +1,1 @@
+#include "testSanitizerUseafterFree.h"
---
+++ testSanitizer/testSanitizerUseafterFree.h
@@ -0,0 +1,14 @@
+#ifndef TEST_SANITIZER_USEAFTER_FREE_H
+#define TEST_SANITIZER_USEAFTER_FREE_H
+
+
+
+void useAfterFree() {
+    
+
+}
+
+
+
+
+#endif // TEST_SANITIZER_USEAFTER_FREE_H
